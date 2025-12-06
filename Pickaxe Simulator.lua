555f6dc7ff3f81297d33883b6c2b5388d2ec28b4
Lick15 <168357577+Lick15@users.noreply.github.com>
2025-12-06 20:42:21 +0800

    Remove unnecessary blank line in Pickaxe Simulator.lua

--- Pickaxe Simulator.lua
+++ Pickaxe Simulator.lua
@@ -1,4 +1,3 @@
-
 if game.PlaceId == 82013336390273 then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
@@ -337,3 +336,4 @@ local DestroyButton = SettingTab:CreateButton({
 })
 
 end
+
